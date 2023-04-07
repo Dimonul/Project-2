78fda4bf2ee8b0548217d42e2c13911ef9c92361
Dimonul <dimpopa@gmail.com>
2023-04-07 16:22:59 +0300

    pagination.categoryId filter

--- script-listing.js
+++ script-listing.js
@@ -7,44 +7,52 @@ function categoriesSlider() {
             slide += '<li class="glide__slide"><a href="?categoryId=' + categorie.id + '"><img src="https://magento-demo.tk/' + categorie.imageUrl + '">' + categorie.name + ' </a></li>';
         });
         document.querySelector('.glide__slides').innerHTML = slide;
+
+        new Glide('.glide', {
+            type: 'carousel',
+            perView: 5,
+            gap: 20,
+            breakpoints: {
+                580: {
+                    perView: 1
+                },
+                768: {
+                    perView: 2
+                },
+                1024: {
+                    perView: 3
+                },
+                1200: {
+                    perView: 4
+                }
+            }
+        }).mount()
     }
 }
 
-const intervalCategorii = setInterval(function() {
-    if (sessionStorage.getItem('categorii')){
-        productsHtml();
+const intervalCategorii = setInterval(function () {
+    if (sessionStorage.getItem('categorii')) {
+        categoriesSlider();
+
         clearInterval(intervalCategorii);
     }
-    console.log('456')}, 200);
-categoriesSlider();
+}, 200);
 
-let ItemsPerPage =sessionStorage.getItem('ItemsPerPage');
-if (ItemsPerPage){
-    document.querySelector('.dropdown-content').value =ItemsPerPage;
+
+let ItemsPerPage = sessionStorage.getItem('ItemsPerPage');
+if (ItemsPerPage) {
+    document.querySelector('.dropdown-content').value = ItemsPerPage;
 }
 document.querySelector('.dropdown-content').addEventListener('change', (event) => {
-sessionStorage.setItem('ItemsPerPage',event.target.value)
+    sessionStorage.setItem('ItemsPerPage', event.target.value)
+    window.location.reload()
+
 });
 
-new Glide('.glide', {
-    type: 'carousel',
-    perView: 5,
-    gap: 20,
-    breakpoints: {
-        580: {
-            perView: 1
-        },
-        768: {
-            perView: 2
-        },
-        1024: {
-            perView: 3
-        },
-        1200: {
-            perView: 4
-        }
-    }
-}).mount()
+
+
+
+
 
 
 
@@ -53,62 +61,72 @@ new Glide('.glide', {
 function productsHtml() {
     let productTemplate = '';
     let finalPrice = '';
-    let saleLabel= '';
+    let saleLabel = '';
+    let items ='';
     let products = JSON.parse(sessionStorage.getItem('products'));
 
-    if (products) {
-        products.forEach(function (product) {
-            const productFinal = product[Object.keys(product)[0]];
+    let categoryId = window.location.search ? window.location.search.replace('?categoryId=', '') : '';  //opertori ternari , daca conditia este adevarata, apelam prima parte, intre semnul intrebarii si :, daca nu e adevarata se va apela ce este dupa :, adica nimic in cazul nostru
 
-            if (Number(productFinal.final_price) < Number(productFinal.price)) {
+    if (categoryId) {
+        items = Object.values(products).filter(function(product) {
 
-                finalPrice = '<p class="finalprice">' + productFinal.final_price + '</p>';
-                saleLabel='<span class="sale">Sale</span>'
-            }
-            else{
-                finalPrice='';
-                saleLabel='';
-            }
-                productTemplate +=
-                    '<div class="card">' +
-                    '<a href="product.html?sku=' + productFinal.sku + '">' +
-                    '<img src="https://magento-demo.tk/media/catalog/product' + productFinal.image + '" alt="' + productFinal.name + '">'+ saleLabel +'</a>' +
-                    '<div class="cardinfo">' +
-                    '<a href="productFinal.html?sku=' + productFinal.sku + '">' +
-                    '<h4>' + productFinal.name + '</h4></a>' +
-                    '<h5>' + productFinal.weight + '</h5>' +
-                    '<div class="card-overlap">' +
-                    '<div class="review">' +
-                    '<i class="stars"></i>' +
-                    '<i class="stars full" style="width: 80%;"></i>' +
-                    '</div>' +
-                    '<div class="price">' +
-                    finalPrice +
-                    '<p class="normalprice">' + productFinal.price + '</p>' +
-                    '</div>' +
-                    '</div >' +
-                    '</div>' +
-                    '</div>';
-        });
-
-        document.querySelector('.fruits').innerHTML = productTemplate;
+        
+        if (Object.values(product)[0].category_id.includes(categoryId)) {
+            return product;
+        }
+        
+        })
+    console.log(items)
+    } else {
+         items =products
     }
+    
+    items.forEach(function (product) {
+        const productFinal = product[Object.keys(product)[0]];
+        if (Number(productFinal.final_price) < Number(productFinal.price)) {
+
+            finalPrice = '<p class="finalprice">' + productFinal.final_price + '</p>';
+            saleLabel = '<span class="sale">Sale</span>'
+        }
+        else {
+            finalPrice = '';
+            saleLabel = '';
+        }
+        productTemplate +=
+            '<div class="card">' +
+            '<a href="product.html?sku=' + productFinal.sku + '">' +
+            '<img src="https://magento-demo.tk/media/catalog/product' + productFinal.image + '" alt="' + productFinal.name + '">' + saleLabel + '</a>' +
+            '<div class="cardinfo">' +
+            '<a href="productFinal.html?sku=' + productFinal.sku + '">' +
+            '<h4>' + productFinal.name + '</h4></a>' +
+            '<h5>' + productFinal.weight + '</h5>' +
+            '<div class="card-overlap">' +
+            '<div class="review">' +
+            '<i class="stars"></i>' +
+            '<i class="stars full" style="width: 80%;"></i>' +
+            '</div>' +
+            '<div class="price">' +
+            finalPrice +
+            '<p class="normalprice">' + productFinal.price + '</p>' +
+            '</div>' +
+            '</div >' +
+            '</div>' +
+            '</div>';
+    });
+
+    document.querySelector('.fruits').innerHTML = productTemplate;
+
+
+
 }
 
-const intervalProduse = setInterval(function() {
-    if (sessionStorage.getItem('products')){
+const intervalProduse = setInterval(function () {
+    if (sessionStorage.getItem('products')) {
         productsHtml();
         clearInterval(intervalProduse);
+
     }
-    console.log('123')}, 200); ///same shit pt categorii
+}, 200);
 
 
 
-// document.addEventListener("produseIncarcate", productsHtml());
-// console.log('123')
-
-   // <span
-    //         class="sale">Sale</span>
-
-
-    //parse array/objects multi dimensional
